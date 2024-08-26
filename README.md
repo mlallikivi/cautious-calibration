784f1acc245149a61a10714c6b001b9e3b31b3c1
mlallikivi <95710779+mlallikivi@users.noreply.github.com>
2024-08-26 12:16:40 +0300

    Added the example and a very short readme.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# cautious-calibration
+# Cautious Calibration in Binary Classification
+
+This repository holds the code for the methods used in the article [Cautious Calibration in Binary Classification](https://arxiv.org/abs/2408.05120) by M-L. Allikivi, J. Järve and M. Kull (2024).
+
+
+The example of how to generate data and how to learn different cautious (and regular) calibration maps on it can be found in [example.ipynb](./src/example.py).
--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,19 @@ def get_isotonic_scores(scores, sequence):
     ir = IsotonicRegression()
     ir.fit(scores, sequence)
     return ir.transform(scores)
+
+
+def make_monotonic(learned_map):
+    monotonic_map = [0] * len(learned_map)
+    monotonic_map[-1] = learned_map[-1]
+    for i in range(len(learned_map) - 2, 0, -1):
+        if learned_map[i] > monotonic_map[i+1]:
+            monotonic_map[i] = monotonic_map[i+1]
+        else:
+            monotonic_map[i] = learned_map[i]
+
+    return monotonic_map
+
+def clip_map(learned_map, threshold):
+    clipped_map = [min(x, threshold) for x in learned_map]
+    return clipped_map
